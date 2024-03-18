80ae2a890c4275f5066a3be71f09c2a69b4ced72
KDT live <dnjsxo0616@gmail.com>
2024-03-18 15:17:39 +0900

    feat: HttpRequest 클래스 업데이트 및 RequestLine 객체 추가
    - RequestLine 클래스를 사용하여 HTTP 요청 라인을 파싱하도록 변경
    - createUser() 메소드를 추가하여 HTTP 요청에서 사용자 정보를 생성하는 기능 구현

--- src/main/java/webserver/HttpRequest.java
+++ src/main/java/webserver/HttpRequest.java
@@ -1,38 +1,37 @@
 package webserver;
 
+import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class HttpRequest {
-    private final String BASE_URL = "src/main/resources/static";
-    private final String requestLine;
+    private final static String BASE_URL = "src/main/resources/static";
+    private final RequestLine requestLine;
     private final List<String> headerLine = new ArrayList<>();
     private static final Logger logger = LoggerFactory.getLogger(HttpRequest.class);
 
     public HttpRequest(InputStream inputStream) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
         String line = br.readLine();
-        this.requestLine = line;
+        this.requestLine = new RequestLine(line);
         while (!(line = br.readLine()).isEmpty()) {
             this.headerLine.add(line);
         }
     }
 
     public void logHeaders() {
-        logger.debug("requestLine" + requestLine);
+        logger.debug("startLine : {} {} {}", requestLine.getHttpMethod(), requestLine.getPath(), requestLine.getProtocol());
         headerLine.forEach(header -> logger.debug("header : {}", header));
     }
 
+
     public byte[] staticFileReader() throws IOException {
-        String[] tokens = requestLine.split(" ");
-        String path = tokens[1];
+        String path = requestLine.getPath();
         String filepath = BASE_URL + path;
 
         try (FileInputStream fileInputStream = new FileInputStream(filepath)) {
@@ -40,4 +39,9 @@ public class HttpRequest {
             return fileInputStream.readAllBytes();
         }
     }
+
+    public User createUser() {
+        return new User(requestLine.getUserId(), requestLine.getName(), requestLine.getEmail(), requestLine.getPassword());
+    }
+
 }
