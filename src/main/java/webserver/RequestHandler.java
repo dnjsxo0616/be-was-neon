0e0e44c0fda49448973ec02f5a07d96f693187d2
KDT live <dnjsxo0616@gmail.com>
2024-03-19 02:57:12 +0900

    feat: HttpResponse 처리 추가
    - HttpRequest 처리 로직에 사용자 생성(/create), 로그인 페이지 요청, 회원가입 페이지 요청 및 기본 페이지 요청에 대한 분기 처리 추가
    - 사용자 생성 시 Database에 사용자 정보 추가 후 전체 사용자 정보 로깅 추가

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -11,6 +11,10 @@ import org.slf4j.LoggerFactory;
 
 public class RequestHandler implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
+    private final static String BASE_URL = "src/main/resources/static";
+    private final static String INDEX_URL = "/index.html";
+    private final static String LOGIN_URL = "/login";
+    public static final String REGISTRATION_URL = "/registration";
 
     private Socket connection;
 
@@ -27,33 +31,31 @@ public class RequestHandler implements Runnable {
             HttpRequest httpRequest = new HttpRequest(in);
             httpRequest.printLog();
 
-            Database.addUser(httpRequest.createUser());
+            if(httpRequest.getPath().contains("/create")) {
+                User user = new User(httpRequest.getUserId(), httpRequest.getName(), httpRequest.getEmail(), httpRequest.getPassword());
+                Database.addUser(user);
 
-            byte[] body = httpRequest.staticFileReader();
+                Collection<User> all = Database.findAll();
+                System.out.println("all = " + all);
 
-            DataOutputStream dos = new DataOutputStream(out);
-            response200Header(dos, body.length);
-            responseBody(dos, body);
-        } catch (IOException e) {
-            logger.error(e.getMessage());
-        }
-    }
+                HttpResponse httpResponse = HttpResponse.createHttpResponse(LOGIN_URL+INDEX_URL);
+                httpResponse.send(out);
 
-    private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
-        try {
-            dos.writeBytes("HTTP/1.1 200 OK \r\n");
-            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
-            dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
-            dos.writeBytes("\r\n");
-        } catch (IOException e) {
-            logger.error(e.getMessage());
-        }
-    }
+            } else if (httpRequest.getPath().equals(LOGIN_URL)) {
+                HttpResponse httpResponse = HttpResponse.staticHttpResponse(new File(BASE_URL + LOGIN_URL + INDEX_URL));
+                httpResponse.send(out);
+            }
+            else if(httpRequest.getPath().equals(REGISTRATION_URL)) {
+                HttpResponse httpResponse = HttpResponse.staticHttpResponse(new File(BASE_URL + REGISTRATION_URL + INDEX_URL));
+                httpResponse.send(out);
+            } else if(httpRequest.getPath().equals("/")) {
+                HttpResponse httpResponse = HttpResponse.staticHttpResponse(new File(BASE_URL + INDEX_URL));
+                httpResponse.send(out);
+            } else {
+                HttpResponse httpResponse = HttpResponse.staticHttpResponse(new File(BASE_URL + httpRequest.getPath()));
+                httpResponse.send(out);
+            }
 
-    private void responseBody(DataOutputStream dos, byte[] body) {
-        try {
-            dos.write(body, 0, body.length);
-            dos.flush();
         } catch (IOException e) {
             logger.error(e.getMessage());
         }
