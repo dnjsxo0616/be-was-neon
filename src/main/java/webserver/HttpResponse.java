504364b9221a3bd8a1d0e3b1f928a5d8bb09cc97
KDT live <dnjsxo0616@gmail.com>
2024-03-21 01:32:31 +0900

    refactor: HttpResponse 클래스 수정 및 재구성
    - HttpResponse 클래스를 수정하여 기존 코드를 개선
    - DataOutputStream을 이용하여 헤더 및 바디를 출력

--- src/main/java/webserver/HttpResponse.java
+++ src/main/java/webserver/HttpResponse.java
@@ -3,16 +3,16 @@ package webserver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.*;
-import java.util.ArrayList;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.util.List;
 
 public class HttpResponse {
 
     private static final Logger logger = LoggerFactory.getLogger(HttpResponse.class);
-    private  final List<String> headers;
     private final byte[] body;
-
+    private final List<String> headers;
 
     public HttpResponse(List<String> headers, byte[] body) {
         this.headers = headers;
@@ -24,87 +24,25 @@ public class HttpResponse {
         this.body = new byte[]{};
     }
 
-    public static HttpResponse staticHttpResponse(File file) {
-        return new HttpResponse(addStaticHeader((int) file.length()), setBody(file));
-    }
-
-    public static HttpResponse createHttpResponse(String location) {
-        return new HttpResponse(addRedirectHeader(location));
-    }
-
-    public static HttpResponse contentTypeHttpResponse(File file, String type) {
-        return new HttpResponse(addContentTypeHeader((int) file.length(), type), setBody(file));
-    }
-
-    private static List<String> addContentTypeHeader(int fileLength, String path) {
-        String type = ContentType.getType(path);
-        List<String> headers = new ArrayList<>();
-        // 상태라인
-        headers.add("HTTP/1.1 200 OK \r\n");
-        // 헤더 작성
-        headers.add("Content-Type: "+type+";charset=utf-8\r\n");
-        headers.add("Content-Length: " + fileLength + "\r\n");
-        headers.add("\r\n");
-        return headers;
-    }
-
-    public static List<String> addStaticHeader(int fileLength) {
-        List<String> headers = new ArrayList<>();
-        // 상태라인
-        headers.add("HTTP/1.1 200 OK \r\n");
-        // 헤더 작성
-        headers.add("Content-Type: text/html;charset=utf-8\r\n");
-        headers.add("Content-Length: " + fileLength + "\r\n");
-        headers.add("\r\n");
-        return headers;
-    }
-
-    public static List<String> addRedirectHeader(String location) {
-        List<String> headers = new ArrayList<>();
-        headers.add( "HTTP/1.1 302 FOUND\r\n");
-        // 헤더 작성
-        headers.add("Location: " + location + "\r\n");
-        headers.add("\r\n");
-        return headers;
-    }
-
-    public static byte[] setBody(File file)  {
-        byte[] body = new byte[(int) file.length()];
-        try(FileInputStream fis = new FileInputStream(file)) {
-            fis.read(body);
-        } catch (IOException e) {
-            logger.error(e.getMessage());
-        }
-        return body;
-    }
-
-    public List<String> getHeaders() {
-        return headers;
-    }
-
-    public byte[] getBody() {
-        return body;
-    }
-
-    public void writeHeader(List<String> headers, DataOutputStream dos) throws IOException {
-        for(String line : headers) {
-            dos.writeBytes(line);
-        }
-    }
-
-    public void writeBody(byte[] body, DataOutputStream dos) throws IOException {
-        dos.write(body, 0, body.length);
-    }
-
-    public void send(OutputStream out){
+    public void send(OutputStream out) throws IOException {
         DataOutputStream dos = new DataOutputStream(out);
         try {
-            writeHeader(getHeaders(), dos);
-            writeBody(getBody(), dos);
+            writeHeader(dos);
+            writeBody(dos);
             dos.flush();
         } catch (IOException e) {
             logger.error(e.getMessage());
         }
+    }
+
+    private void writeHeader(DataOutputStream dos) throws IOException {
+        for(String header : headers) {
+            dos.writeBytes(header);
+        }
+    }
+
+    private void writeBody(DataOutputStream dos) throws IOException {
+        dos.write(body, 0, body.length);
     }
 
 }
