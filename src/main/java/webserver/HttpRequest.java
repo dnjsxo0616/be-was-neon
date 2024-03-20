2b5426411fd807bed54ff419c4bdd7c770e86488
KDT live <dnjsxo0616@gmail.com>
2024-03-21 01:25:21 +0900

    refactor: HttpRequest 리팩토링
    - HTTP 요청의 정보를 받아들이는 방식이 변경되어, 생성자에서는 HTTP 요청과 관련된 정보들만을 받아들이도록 변경
    - 기존의 startLine과 headerLine 필드를 httpMethod, requestTarget, headers 필드로 변경
    - header List<String> -> Map<String, String>

--- src/main/java/webserver/HttpRequest.java
+++ src/main/java/webserver/HttpRequest.java
@@ -1,71 +1,30 @@
 package webserver;
 
-import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.*;
-import java.net.URLDecoder;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class HttpRequest {
     private static final Logger logger = LoggerFactory.getLogger(HttpRequest.class);
-    private final String startLine;
-    private final List<String> headerLine = new ArrayList<>();
-    private final Map<String, String> params = new HashMap<>();
+    private final String httpMethod;
+    private final String requestTarget;
+    private final Map<String, String> headers;
+    private final Map<String, String> params;
 
-    public HttpRequest(InputStream inputStream) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"));
-        String line = br.readLine();
-        this.startLine = line;
-        if (getPath().contains("?")) {
-            String[] pathPart = getPath().split("\\?");
-            if (pathPart.length == 2) {
-                parseParams(pathPart);
-            }
-        }
-        while (!(line = br.readLine()).isEmpty()) {
-            this.headerLine.add(line);
-        }
+    public HttpRequest(String httpMethod, String requestTarget, Map<String, String> headers) {
+        this.httpMethod = httpMethod;
+        this.requestTarget = requestTarget;
+        this.headers = headers;
+        this.params = new HashMap<>();
     }
 
-    public void printLog() {
-        logger.debug("startLine : {}", startLine);
-        headerLine.forEach(header -> logger.debug("header : {}", header));
-    }
-
-    public String getHttpMethod() {
-        String[] startPart = parseStartLine(startLine);
-        return startPart[0];
-    }
-
-    public String getPath() {
-        String[] startPart = parseStartLine(startLine);
-        return startPart[1];
-    }
-
-    private String[] parseStartLine(String line) {
-        return line.split(" ");
-    }
-
-
-    private void parseParams(String[] pathPart) {
-        String[] paramArray = pathPart[1].split("&");
-        for (String param : paramArray) {
-            String[] paramKeyValue = param.split("=");
-            if (paramKeyValue.length == 2) {
-                try {
-                    String key = URLDecoder.decode(paramKeyValue[0], "UTF-8");
-                    String value = URLDecoder.decode(paramKeyValue[1], "UTF-8");
-                    params.put(key, value);
-                } catch (UnsupportedEncodingException e) {
-                    logger.error(e.getMessage());
-                }
-            }
-        }
+    public HttpRequest(String httpMethod, String requestTarget, Map<String, String> headers, Map<String, String> param) {
+        this.httpMethod = httpMethod;
+        this.requestTarget = requestTarget;
+        this.headers = headers;
+        this.params = param;
     }
 
     public String getUserId() {
@@ -84,5 +43,11 @@ public class HttpRequest {
         return params.get("password");
     }
 
+    public String getHttpMethod() {
+        return httpMethod;
+    }
 
+    public String getRequestTarget() {
+        return requestTarget;
+    }
 }
