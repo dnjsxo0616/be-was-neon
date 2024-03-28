85189669c4905557fc1f6fcb0cdb04d2305d1bb6
KDT live <dnjsxo0616@gmail.com>
2024-03-28 16:22:11 +0900

    refactor: redirect location 변경
    - index.html -> /main/index.html

--- src/main/java/webserver/response/CreateHeader.java
+++ src/main/java/webserver/response/CreateHeader.java
@@ -45,8 +45,8 @@ public class CreateHeader {
     public List<String> loginSuccessWithCookie(String cookieValue) {
         List<String> headers = new ArrayList<>();
         headers.add(REDIRECTION_302);
-        headers.add("Location: index.html\r\n");
-        headers.add("Set-Cookie: session=" + cookieValue+ "\r\n");
+        headers.add("Location: /main/index.html\r\n");
+        headers.add("Set-Cookie: session=" + cookieValue+ "; Path=/\r\n");
         headers.add("Content-Type: text/html;charset=utf-8\r\n");
         headers.add("\r\n");
         return headers;
