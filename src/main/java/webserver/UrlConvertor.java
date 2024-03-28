3aa9cf14aec2fe8f11cf759d31d7a308748852c8
KDT live <dnjsxo0616@gmail.com>
2024-03-28 14:13:59 +0900

    refactor: UrlConvertor에 로그인 실패 페이지 URL 상수 추가
    - 로그인 실패 시 사용할 페이지 URL을 상수로 추가함(LOGIN_FAIL_URL).
    - CREATE_URL 상수 삭제

--- src/main/java/webserver/UrlConvertor.java
+++ src/main/java/webserver/UrlConvertor.java
@@ -5,14 +5,16 @@ import java.io.File;
 public class UrlConvertor {
     private final static String BASE_URL = "src/main/resources/static";
     public final static String INDEX_URL = "/index.html";
-    public final static String CREATE_URL = "/create";
+    public final static String LOGIN_FAIL_URL = "/login/login_failed.html";
 
     public String urlController(String requestTarget) {
+        // 기본 경로 추가
         StringBuilder sb = new StringBuilder(BASE_URL);
         sb.append(requestTarget);
 
         File file = new File(sb.toString());
 
+        // 디렉토리면 index.html 추가
         if (file.isDirectory()) {
             sb.append(INDEX_URL);
         }
