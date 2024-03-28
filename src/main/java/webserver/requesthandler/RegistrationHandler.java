c2c9e66baf388e5c579037b91164232e394e24aa
KDT live <dnjsxo0616@gmail.com>
2024-03-28 14:16:08 +0900

    feat: RegistrationHandler에 GET 요청 처리 기능 추가
    - 파일 경로 확인 및 파일 읽기 로직을 포함한 GET 요청 처리 기능을 추가
    - 요청된 URL에 해당하는 파일이 존재하는 경우, 해당 파일의 내용을 반환하고, 존재하지 않는 경우 404 Not Found 응답을 반환
    - 파일을 성공적으로 읽었을 경우 200 OK 응답과 함께 파일 내용을 반환하고, 파일 읽기 과정에서 오류가 발생한 경우 500 Server Error 응답을 반환

--- src/main/java/webserver/requesthandler/RegistrationHandler.java
+++ src/main/java/webserver/requesthandler/RegistrationHandler.java
@@ -9,6 +9,7 @@ import webserver.request.HttpRequest;
 import webserver.response.CreateHeader;
 import webserver.response.HttpResponse;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.Map;
 
@@ -18,8 +19,29 @@ public class RegistrationHandler implements Handler {
 
     @Override
     public HttpResponse getRequest(HttpRequest httpRequest) {
+        String requestTarget = httpRequest.getRequestTarget();
         CreateHeader createHeader = new CreateHeader();
-        return new HttpResponse(createHeader.notFound404());
+        UrlConvertor urlConvertor = new UrlConvertor();
+
+        String filePath = urlConvertor.urlController(requestTarget);
+        String type = urlConvertor.typeController(requestTarget);
+
+        File file = new File(filePath);
+        // 파일이 존재하는지
+        if (file.exists()) {
+            try {
+                FileReader fileReader = new FileReader();
+                byte[] body = fileReader.responseBody(file);
+                return new HttpResponse(createHeader.generate200((int) file.length(), type), body);
+            } catch (Exception e) {
+                logger.error(e.getMessage());
+                // 파일을 읽다가 오류 발생하면 500 응답 반환
+                return new HttpResponse(createHeader.serverError500());
+            }
+        } else {
+            // 요청된 파일이 존재하지 않는 경우 404 응답 반환
+            return new HttpResponse(createHeader.notFound404());
+        }
     }
 
     @Override
